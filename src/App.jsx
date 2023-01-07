e066c19d035be0a9a522852dafa554de54977eaa
fernandog197 <bluewavesalta@gmail.com>
2023-01-06 21:30:05 -0300

    v0.0.3

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Home from './components/Home/Home'
 import About from './components/About/About'
 import Skills from './components/Skills/Skills'
 import Services from './components/services/Services'
+import Qualification from './components/qualification/Qualification'
 
 function App() {
 
@@ -18,6 +19,7 @@ function App() {
         <About />
         <Skills />
         <Services />
+        <Qualification />
       </main>
     </>
   )
