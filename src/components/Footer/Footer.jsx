d1cbca9e04c4a205cf008a1c9105ce0151e627e9
fernandog197 <bluewavesalta@gmail.com>
2023-02-07 07:45:51 -0300

    Correccion menor footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
     return (
         <footer className='footer'>
             <div className="footer__container container">
-                <h1 className="footer__title">Miguel</h1>
+                <h1 className="footer__title">Miguel Garcia</h1>
 
                 <ul className="footer__list">
                     <li>
